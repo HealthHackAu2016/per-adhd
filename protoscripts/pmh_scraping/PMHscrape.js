4d9d69f09e69af5726032ad04db2aeb979cecf24
heilar <rowanashwin@hotmail.com>
2016-10-15 17:45:14 +0800

    got it printing full pdfs worth of info

--- protoscripts/pmh_scraping/PMHscrape.js
+++ protoscripts/pmh_scraping/PMHscrape.js
@@ -8,6 +8,29 @@
   require('pdfjs-dist');
 
 
+  function processPdf(pdf){
+    console.log(pdf.pdfInfo.numPages);
+    var fullString = 'start '; //fix this - turn into a string builder
+
+    var pagePromises = [];
+    var pageStrings=[];
+    for(var i = 2; i < pdf.pdfInfo.numPages; i++){
+      console.log(i);
+      pagePromises.push(pdf.getPage(i)
+        .then( (page) => page.getTextContent())
+        .then( function(content) {
+          var pageString = 'pagestart ';
+          content.items.forEach((item) => pageString += item.str);
+          pageStrings.push(pageString);
+        })
+      );
+    }
+
+    return Promise.all(pagePromises).then(()=>{
+      console.log(pageStrings);
+      return pageStrings});
+}
+
   function loadSiteList() {
     var fn = path.resolve(__dirname, 'pmh_sites.json');
     return JSON.parse(fs.readFileSync(fn, 'utf8'));
@@ -22,13 +45,8 @@
     var url = siteList[occupation];
     downloadedPromises[occupation] = download(url).then(
       function(pdfStream) {
-        console.log(pdfStream.length);
         return PDFJS.getDocument(pdfStream)
-          .then( (pdf) => pdf.getPage(1))
-          .then( (page) => page.getTextContent())
-          .then( function(content) {
-            console.log(content) //.items.map( (item) => item.str)
-          })
+          .then( (pdf) => processPdf(pdf) )
         }
     );
   }
