6d03955d9e3666f2e5d56f70aaeeacc1903ddda9
Frames <oxinabox@ucc.asn.au>
2016-10-15 08:30:13 +0000

    =A pdf scraper that works

--- protoscripts/pmh_scraping/PMHscrape.js
+++ protoscripts/pmh_scraping/PMHscrape.js
@@ -1,17 +1,11 @@
+// vim: ts=2: sw=2: set expandtab:
 (function(){
-	"use strict";
+"use strict";
   var download = require('download');
-	var fs = require('fs');
+  var fs = require('fs');
   var path = require('path');
 
-  //C:\Users\rowan\IdeaProjects\per-adhd\node_modules\pdf.js
-  // require.config({paths: {'pdfjs': '../../src'}});
-  // require(['pdfjs/display/api', 'pdfjs/display/global'], function (api, global) {
-  //   // In production, change this to point to the built `pdf.worker.js` file.
-  //   global.PDFJS.workerSrc = '../../src/worker_loader.js';
-  // })
-
-  var PDFParser = require('pdfreader');
+  require('pdfjs-dist');
 
 
   function loadSiteList() {
@@ -24,14 +18,17 @@
   var downloadedPromises = {};
   var occupations = [];
   for(var occupation in siteList){
-    if(!siteList.hasOwnProperty(occupation))continue;
+    if(!siteList.hasOwnProperty(occupation)) continue;
     var url = siteList[occupation];
     downloadedPromises[occupation] = download(url).then(
       function(pdfStream) {
         console.log(pdfStream.length);
-        return pdfParser.getDocument(pdfStream)
-          .then( (pdf) => pdf.parseFileItem(1))
+        return PDFJS.getDocument(pdfStream)
+          .then( (pdf) => pdf.getPage(1))
           .then( (page) => page.getTextContent())
+          .then( function(content) {
+            console.log(content) //.items.map( (item) => item.str)
+          })
         }
     );
   }
